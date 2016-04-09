db7958fba258b9e94b9f0cbfa4d0e32847397a57
pedrocb <pedrocb_c-s@hotmail.com>
2016-04-09 03:20:01 +0100

    Greedy Search (pesquisa sôfrega) implemented

--- Sokoban Game/Assets/Scripts/SearchProblem.cs
+++ Sokoban Game/Assets/Scripts/SearchProblem.cs
@@ -4,26 +4,28 @@ using System.Collections;
 
 public struct Successor
 {
-	public object state;
-	public float cost;
-	public Action action;
+    public object state;
+    public float cost;
+    public Action action;
 
 
-	public Successor(object state, float cost, Action a)
-	{
-		this.state = state;
-		this.cost = cost;
-		this.action = a;
-	}
+    public Successor(object state, float cost, Action a)
+    {
+	this.state = state;
+	this.cost = cost;
+	this.action = a;
+    }
 }
 
 
 public interface ISearchProblem
 {
-	object GetStartState ();
-	bool IsGoal (object state);
-	Successor[] GetSuccessors (object state);
+    object GetStartState ();
+    bool IsGoal (object state);
+    Successor[] GetSuccessors (object state);
+    int BoxesMissing(object state);
 
-	int GetVisited ();
-	int GetExpanded ();
+    int GetVisited ();
+    int GetExpanded ();
 }
+ 
