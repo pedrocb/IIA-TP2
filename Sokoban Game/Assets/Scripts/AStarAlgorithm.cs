df0d1ef6f57ab9e9df5009018fcc1ccc993e09dc
pedrocb <pedrocb_c-s@hotmail.com>
2016-04-14 15:19:27 +0100

    fixed A*

--- Sokoban Game/Assets/Scripts/AStarAlgorithm.cs
+++ Sokoban Game/Assets/Scripts/AStarAlgorithm.cs
@@ -4,66 +4,55 @@ using System.Collections.Generic;
 
 public class AStarAlgorithm : SearchAlgorithm {
 
-	private List<SearchNode> openList = new List<SearchNode> ();
-	private HashSet<object> closedSet = new HashSet<object> ();
+    private List<SearchNode> openList = new List<SearchNode> ();
+    private HashSet<object> closedSet = new HashSet<object> ();
 
-	void Start () 
-	{
-		problem = GameObject.Find ("Map").GetComponent<Map> ().GetProblem();
-		SearchNode start = new SearchNode (problem.GetStartState (), 0);
-		openList.Add (start);
-	}
+    void Start () 
+    {
+	problem = GameObject.Find ("Map").GetComponent<Map> ().GetProblem();
+	SearchNode start = new SearchNode (problem.GetStartState (), 0);
+	openList.Add (start);
+    }
 
-	protected override void Step()
-	{
-		Debug.Log(openList.Count);
-		if (openList.Count > 0)
-		{
-			openList.Sort (compareFunction);
-			SearchNode cur_node = openList[0];
-			openList.RemoveAt(0);
-			closedSet.Add (cur_node.state);
+    protected override void Step()
+    {
+	if (openList.Count > 0)
+	    {
+		SearchNode cur_node = openList[0];
+		openList.RemoveAt(0);
+		closedSet.Add (cur_node.state);
 
-			if (problem.IsGoal (cur_node.state)) {
-				solution = cur_node;
-				finished = true;
-				running = false;
-			} else {
-				Successor[] sucessors = problem.GetSuccessors (cur_node.state);
-				foreach (Successor suc in sucessors) {
-					if (!closedSet.Contains (suc.state)) {
-						SearchNode new_node = new SearchNode (suc.state, suc.cost + cur_node.g, problem.BoxesMissing(cur_node.state), suc.action, cur_node);	
-						openList.Add (new_node);
-						//insertNode(new_node);
-					}
-				}
-			}
-		}
-		else
-		{
-			finished = true;
-			running = false;
-		}
-	}
-
-	public void insertNode(SearchNode node){
-		for(int i=0;i<openList.Count;i++){
-			if(node.f<openList[i].f){
-				openList.Insert(i,node);
-				return;
-			}
-
-		}
-		openList.Add(node);
-	}
-
-
-	private static int compareFunction(SearchNode a, SearchNode b)
-	{
-		if (a.f >= b.f) {
-			return 1;
+		if (problem.IsGoal (cur_node.state)) {
+		    solution = cur_node;
+		    finished = true;
+		    running = false;
 		} else {
-			return -1;
+		    Successor[] sucessors = problem.GetSuccessors (cur_node.state);
+		    foreach (Successor suc in sucessors) {
+			if (!closedSet.Contains (suc.state)) {
+			    SearchNode new_node = new SearchNode (suc.state, suc.cost + cur_node.g, problem.BoxesMissing(suc.state), suc.action, cur_node);	
+			    insertNode(new_node);
+			}
+		    }
 		}
+	    }
+	else
+	    {
+		finished = true;
+		running = false;
+	    }
+    }
+		
+    
+    public void insertNode(SearchNode node){
+	for(int i=0;i<openList.Count;i++){
+	    if(node.f<openList[i].f){
+		openList.Insert(i,node);
+		return;
+	    }
+
 	}
+	openList.Add(node);
+    }
+
 }
--- Sokoban Game/Assets/Scripts/AprofundamentoProgressivo.cs
+++ Sokoban Game/Assets/Scripts/AprofundamentoProgressivo.cs
@@ -4,57 +4,57 @@ using System.Collections.Generic;
 
 public class AprofundamentoProgressivo : SearchAlgorithm {
 
-	private Stack<SearchNode> stack = new Stack<SearchNode>();
+    private Stack<SearchNode> stack = new Stack<SearchNode>();
     private HashSet<object> closedSet = new HashSet<object> ();
     public int maximumDepth;
-	// Use this for initialization
-	void Start () {
-		problem = GameObject.Find ("Map").GetComponent<Map> ().GetProblem();
+    // Use this for initialization
+    void Start () {
+	problem = GameObject.Find ("Map").GetComponent<Map> ().GetProblem();
 
+    }
+
+    protected override void Step()
+    {
+	while(!depthFirstSearch()){
+	    maximumDepth++;
 	}
 
-	protected override void Step()
-	{
-		while(!depthFirstSearch()){
-			maximumDepth++;
-		}
+    }
 
-	}
+    bool depthFirstSearch()
+    {
+	closedSet.Clear();
+	stack.Clear();
 
-	bool depthFirstSearch()
-	{
-		closedSet.Clear();
-		stack.Clear();
+	SearchNode start = new SearchNode (problem.GetStartState (), 0);
+	stack.Push(start);
+	while (stack.Count > 0)
+	    {
+		SearchNode cur_node = stack.Pop();
+		closedSet.Add (cur_node.state);
 
-		SearchNode start = new SearchNode (problem.GetStartState (), 0);
-		stack.Push(start);
-		while (stack.Count > 0)
-		    {
-			SearchNode cur_node = stack.Pop();
-			closedSet.Add (cur_node.state);
+		if (problem.IsGoal (cur_node.state)) {
+		    solution = cur_node;
+		    finished = true;
+		    running = false;
+		    return true;
+		} else if(cur_node.depth < maximumDepth){
+		    Successor[] sucessors = problem.GetSuccessors (cur_node.state);
+		    foreach (Successor suc in sucessors) {
+			if (!closedSet.Contains (suc.state)) {
+			    SearchNode new_node = new SearchNode (suc.state, suc.cost + cur_node.g, suc.action, cur_node);
 
-			if (problem.IsGoal (cur_node.state)) {
-			    solution = cur_node;
-			    finished = true;
-			    running = false;
-				return true;
-			} else if(cur_node.depth < maximumDepth){
-			    Successor[] sucessors = problem.GetSuccessors (cur_node.state);
-			    foreach (Successor suc in sucessors) {
-				if (!closedSet.Contains (suc.state)) {
-				    SearchNode new_node = new SearchNode (suc.state, suc.cost + cur_node.g, suc.action, cur_node);
-
-				    stack.Push(new_node);
-				}
-			    }
+			    stack.Push(new_node);
 			}
 		    }
-
-			finished = true;
-			running = false;
-
-			return false;
+		}
 	    }
+
+	finished = true;
+	running = false;
+
+	return false;
+    }
 
 
 }
